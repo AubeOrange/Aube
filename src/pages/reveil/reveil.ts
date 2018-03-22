fd7ab68f0deaa980b6ca79252ae5601388f68259
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2018-03-22 18:27:10 +0100

    pute

--- src/pages/reveil/reveil.ts
+++ src/pages/reveil/reveil.ts
@@ -30,68 +30,25 @@ export class ReveilPage {
     constructor(public navCtrl: NavController, public navParams: NavParams, private localNotification:LocalNotifications, private alertCtrl:AlertController, private platform:Platform) {
         this.notificationTime = moment(new Date()).format();
 
-        this.days = [
-            { title: 'Lundi', dayId: 1, checked: false},
-            { title: 'Mardi', dayId: 2, checked: false},
-            { title: 'Mercredi', dayId: 3, checked: false},
-            { title: 'Jeudi', dayId: 4, checked: false},
-            { title: 'Vendredi', dayId: 5, checked: false},
-            { title: 'Samedi', dayId: 6, checked: false},
-            { title: 'Dimanche', dayId: 0, checked: false},
-        ];
-    }
-
-    ngOnInit(){
-        this.platform.ready().then(result => {
-            this.localNotification.getAllScheduled().then(data => {
-                this.notifications = data;
+        this.platform.ready().then((ready) => {
+            this.localNotification.on('click', (notification, state) => {
+                let alert = this.alertCtrl.create({
+                    title: 'test',
+                    subTitle: 'notif',
+                });
+                
+                alert.present();
             });
         });
     }
 
-    timeChange(time){
-        this.notifHour = time.hour;
-        this.notifMinute = time.minute;
-    }
-
-
     scheduleNotification(){
-        let currentDate = new Date();
-        let currentDay = currentDate.getDay();
-
-        for(let day of this.days) {
-            if(day.checked){
-                let firstNotificationTime = new Date();
-                let dayDifference = day.dayId - currentDay;
-
-                if(dayDifference < 0){
-                    dayDifference += 7;
-                }
-                firstNotificationTime.setHours(this.notifHour);
-                firstNotificationTime.setMinutes(this.notifMinute);
-                firstNotificationTime.setDate(firstNotificationTime.getDate() + dayDifference);
-
-                let notification = {
-                    id: day.dayId,
-                    title: 'Hey! Debout la dedans',
-                    text: 'Bon reveil avec Aube',
-                    sound: this.platform.is('android') ? 'file://assets/sounds/secondes.mp3' : 'file://assests/sounds/secondes.caf',
-                    at: firstNotificationTime,
-                    every: 'week'
-                };
-
-                this.notifications.push(notification);
-            }
-        }
-
-        for(let notification of this.notifications){
-            this.localNotification.schedule(notification);
-        }
-    }
-
-    clearNotifications(){
-        this.localNotification.cancelAll().then(() => {
-            this.notifications = [];
+        console.log('notif schedule');
+        this.localNotification.schedule({
+            id: 1,
+            title: 'Hey! Debout la dedans',
+            text: 'Bon reveil avec Aube',
+            at: new Date(new Date().getTime() + 5 * 1000),
         });
     }
 }
