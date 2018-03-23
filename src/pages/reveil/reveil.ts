213fa4a2daf6517d1cca5bdf6b6c25aeaef53f8a
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2018-03-23 16:35:01 +0100

    last commit

--- src/pages/reveil/reveil.ts
+++ src/pages/reveil/reveil.ts
@@ -25,26 +25,119 @@ export class ReveilPage {
 
     notifications: any[] = [];
 
-    scheduledNotifications: any[] = [];
+    scheduledNotifications = [];
+
+    selectedDay: {title: string, dayId:number};
 
     constructor(public navCtrl: NavController, public navParams: NavParams, private localNotification:LocalNotifications, private alertCtrl:AlertController, private platform:Platform) {
         this.notificationTime = moment(new Date()).format();
 
-        this.platform.ready().then((ready) => {
-            this.localNotification.on('click', (notification, state) => {
-                this.navCtrl.setRoot(ChatPage, {mydata: "data"});
-            });
+        this.days = [
+            { title: 'Lundi', dayId: 1},
+            { title: 'Mardi', dayId: 2},
+            { title: 'Mercredi', dayId: 3},
+            { title: 'Jeudi', dayId: 4},
+            { title: 'Vendredi', dayId: 5},
+            { title: 'Samedi', dayId: 6},
+            { title: 'Dimanche', dayId: 0},
+        ];
+
+    }
+
+    ngOnInit(){
+        this.localNotification.on('click', (notification, state) => {
+            this.navCtrl.setRoot(ChatPage, {mydata: "data"});
         });
+
+        if(this.platform.is('cordova')) {
+            this.getScheduledAlarms();
+        }
+    }
+
+    getScheduledAlarms(){
+            this.localNotification.getAllScheduled().then(data => {
+                this.scheduledNotifications = [];
+                for (let alrm of data) {
+                    console.log(new Date(alrm.at).toLocaleDateString('fr-FR', {weekday: 'long', hour: 'numeric', minute: 'numeric'}));
+                    this.scheduledNotifications.push(new Date(alrm.at).toLocaleDateString('fr-FR', {weekday: 'long', hour: 'numeric', minute: 'numeric'}));
+                }
+                console.log(this.scheduledNotifications);
+            });
+    }
+
+    onSelectDay(day){
+        console.log(day);
+        this.selectedDay = day;
+    }
+
+    timeChange(time){
+        this.notifHour = time.hour;
+        this.notifMinute = time.minute;
     }
 
     scheduleNotification(){
-        console.log('notif schedule');
-        this.localNotification.schedule({
-            id: 1,
-            title: 'Hey! Debout la dedans',
-            text: 'Bon reveil avec Aube',
-            sound: this.platform.is('android') ? 'file://assets/sounds/secondes.mp3' : 'file://assets/sounds/secondes.caf',
-            at: new Date(new Date().getTime() + 5 * 1000),
-        });
+        if(this.selectedDay !== undefined){
+            if (this.notifHour !== undefined && this.notifMinute !== undefined){
+                let currentDate = new Date();
+                let currentDay = currentDate.getDay();
+
+                let firstNotificationTime = new Date();
+                let dayDifference = this.selectedDay.dayId - currentDay;
+
+                if(dayDifference < 0){
+                    dayDifference += 7;
+                }
+
+                firstNotificationTime.setHours(this.notifHour);
+                firstNotificationTime.setMinutes(this.notifMinute);
+                firstNotificationTime.setDate(firstNotificationTime.getDate() + dayDifference);
+
+                let notification = {
+                    id: this.selectedDay.dayId,
+                    title: 'Hey! Debout la dedans',
+                    text: 'Bon reveil avec Aube',
+                    sound: this.platform.is('android') ? 'file://assets/sounds/secondes.mp3' : 'file://assets/sounds/secondes.caf',
+                    at: firstNotificationTime,
+                    every: 'week'
+                };
+
+                console.log(firstNotificationTime);
+
+
+                if( this.platform.is('cordova')){
+                    this.localNotification.schedule(notification);
+                }
+
+                let alert = this.alertCtrl.create({
+                    title: 'Youpi',
+                    subTitle:'Un nouveau reveil est programmé tous les '+this.selectedDay.title+' à '+this.notifHour+':'+this.notifMinute,
+                    buttons: ['Ok']
+                });
+
+                alert.present();
+
+                if(this.platform.is('cordova')) {
+                    this.getScheduledAlarms();
+                }
+            }
+            else {
+                let alert = this.alertCtrl.create({
+                    title: 'Oups',
+                    subTitle: "Veuiller selectionner l'heure du reveil",
+                    buttons: ['Ok']
+                });
+
+                alert.present();
+            }
+        }
+        else{
+            let alert = this.alertCtrl.create({
+                title: 'Oups',
+                subTitle:"Vous n'avez pas selectionné de jour",
+                buttons: ['Ok']
+            });
+
+            alert.present();
+        }
     }
 }
